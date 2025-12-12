f6550890b19258c6a3187718ba6f567d32d2903d
wilma <mornlitoralis@gmail.com>
2025-12-12 17:52:54 +0700

    add file

---
+++ php/tugas/README.md
@@ -0,0 +1,7 @@
+# Tugas php
+
+## Tugas8
+
+1. Buat variabel $jam (misal $jam = 13;).
+2. Gunakan struktur if, elseif, dan else untuk menentukan kategori waktu.
+3. Tampilkan hasil menggunakan echo.
---
+++ php/tugas/tugas8.php
@@ -0,0 +1,17 @@
+<?php
+    $jam = 7.00;
+
+    echo "Jam saat ini: $jam.00\n";
+    
+    if ($jam >= 5.00 && $jam <= 10.59 ){
+        echo"Itu pagi bos dari jam (05.00-10.59)\n";
+    }else if ($jam >= 11.00 && $jam <= 14.59 ){
+        echo "Siang mas, bangun. Makan dulu sana! (11.00 - 14.59)\n";
+    }
+     else if ($jam >= 15.00 && $jam <= 17.59){
+        echo "Udah sore bang, pulang. (15.00-17.59)\n";
+    }else if ($jam >= 18.00 && $jam <= 04.59){
+        echo "Malem bang saatnya tidur. (18.00 - 04.59)\n";
+    }
+
+?>
